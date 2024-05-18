8128756ab849a553eb46dfe0bae4851a635e45d4
hapo-linhptm <linh.ptm@haposoft.com>
2024-05-18 21:00:54 +0700

    lesson-135: fix

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,5 +1,9 @@
-export default function NewsDetailPage() {
+export default function NewsDetailPage({ params }) {
+  const newId = params.slug
   return (
-    <h1>this is news page detail</h1>
+    <>
+      <h1>this is news page detail</h1>
+      <h2>slug { newId }</h2>
+    </>
   );
 }
